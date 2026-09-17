9ce21ae9db3acd62f18230f5dd2553214f2d2b8b
pirahanmardane <pirahanemardane@gmail.com>
2026-09-17 06:02:22 +0330

    fix: import Card and related UI modules in ProductCard1

--- src/components/ui/product-card-1.tsx
+++ src/components/ui/product-card-1.tsx
@@ -1,4 +1,9 @@
 "use client";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+
 
 import { useState } from "react";
 
